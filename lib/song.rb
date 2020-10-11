ac64ac54289cebb9c9a70bab8ce9b9ddcded734e
gamil91 <alexis_gamil99@yahoo.com.com>
2020-10-11 14:10:05 -0700

    what a lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,9 @@
+require 'pry'
+
 class Song
+  
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
@@ -9,5 +13,60 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    self.all << self.new
+    self.all.last
+    
+  end
+
+  def self.new_by_name(name)
+      song = self.new
+      song.name = name
+      song
+  end
+
+  def self.create_by_name(name)
+      song = self.create
+      song.name = name
+      song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name) 
+    else
+      self.find_by_name(name)
+    end
+  end
+
+    def self.alphabetical
+      self.all.sort_by {|song| song.name }
+    end
+
+    def self.new_from_filename(file)
+      arr = file.split(" - ")
+      name = arr[1].delete_suffix(".mp3")
+      artist = arr[0]
+
+      song = self.new
+      song.name = name
+      song.artist_name = artist
+      song
+    end
+
+    def self.create_from_filename(file)
+     self.all << self.new_from_filename(file)
+    end
+
+    def self.destroy_all
+      self.all.clear
+    end
+
+
 
 end
